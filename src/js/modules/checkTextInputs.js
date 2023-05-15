c25948fa33110cc1fb6746c76735651a447907df
Konstantin0 <maschkovc@yandex.ru>
2023-05-15 19:47:46 +0300

    валидация

--- src/js/modules/checkTextInputs.js
+++ src/js/modules/checkTextInputs.js
@@ -5,10 +5,14 @@ const checkTextInputs = (selector) => {
         input.addEventListener('keypress', function(e) {
             if(e.key.match(/[^а-яё 0-9]/ig)) {
                 e.preventDefault();
-            }
+            } 
         });
-    });
+        input.addEventListener('blur input', function(e) {
+            e.preventDefault();
+            // input.value.replace(/[a-zA-Z]/ig, "");
+        });
 
+    });
 };
 
 export default checkTextInputs;
