53fa46b265c1f5f544a4285563f34ce162512899
Konstantin0 <maschkovc@yandex.ru>
2023-05-16 10:24:43 +0300

    6. подгрузка доп стилей

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import sliders from "./modules/sliders";
 import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
+import showMoStyles from "./modules/showMoStyles";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -14,6 +15,7 @@ window.addEventListener("DOMContentLoaded", () => {
     mask("[name='phone']");
     checkTextInputs("[name='name']");
     checkTextInputs("[name='message']");
+    showMoStyles(".button-styles", ".styles-2");
 
     
 });
